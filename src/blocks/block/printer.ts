5a41c9ee39d8ac7e5e94c8752e61ee36690f5f3d
Oliver Wagner <11217061+rogervanwile@users.noreply.github.com>
2022-07-30 16:37:32 +0200

    Fix "undefined" string when printing recipe

--- src/blocks/block/printer.ts
+++ src/blocks/block/printer.ts
@@ -71,24 +71,24 @@ export class Printer {
           <div class="title">
             <div class="left">
               <div class="topline">${topline}</div>
-              ${titleContainer?.outerHTML}
+              ${titleContainer?.outerHTML || ''}
             </div>
             <div class="right">
-              ${svgStringQrCode}
-              <p class="qr-hint"><small>Link zum Rezept</small></p>
+            ${svgStringQrCode || ''}
+            <p class="qr-hint"><small>Link zum Rezept</small></p>
             </div>
           </div>
           <header>
-            ${introContainer?.outerHTML}
-            ${timingListContainer?.outerHTML}
+            ${introContainer?.outerHTML || ''}
+            ${timingListContainer?.outerHTML || ''}
           </header>
-          ${imageContainer?.outerHTML}
+          ${imageContainer?.outerHTML || ''}
           <aside>
-            ${ingredientsContainer?.outerHTML}
+            ${ingredientsContainer?.outerHTML || ''}
           </aside>
           <main>
-            ${preparationStepsContainer?.outerHTML}
-            ${notesContainer?.outerHTML}
+            ${preparationStepsContainer?.outerHTML || ''}
+            ${notesContainer?.outerHTML || ''}
           </main>
           <footer>
             <div class="footer-inner">
