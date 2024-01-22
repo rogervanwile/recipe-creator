0cd7bf3cdf9279200f45e7aa77c713ce70b9b3fe
Oliver Wagner <11217061+rogervanwile@users.noreply.github.com>
2024-01-22 22:54:23 +0100

    Replace PHP Shortcodes

--- templates/admin-index-page.php
+++ templates/admin-index-page.php
@@ -9,24 +9,24 @@ $recipeCreator = new RecipeCreator(); ?>
     <div id="col-left">
       <div class="col-wrap">
         <form method="post" action="options.php" id="recipe-creator--settings-form">
-          <input type="hidden" name="recipe_creator__primary_color_contrast" value="<?= esc_attr(
-              get_option("recipe_creator__primary_color_contrast")
-          ) ?>" />
-          <input type="hidden" name="recipe_creator__primary_color_light" value="<?= esc_attr(
-              get_option("recipe_creator__primary_color_light")
-          ) ?>" />
-          <input type="hidden" name="recipe_creator__primary_color_light_contrast" value="<?= esc_attr(
-              get_option("recipe_creator__primary_color_light_contrast")
-          ) ?>" />
-          <input type="hidden" name="recipe_creator__primary_color_dark" value="<?= esc_attr(
-              get_option("recipe_creator__primary_color_dark")
-          ) ?>" />
-          <input type="hidden" name="recipe_creator__secondary_color_contrast" value="<?= esc_attr(
-              get_option("recipe_creator__secondary_color_contrast")
-          ) ?>" />
-          <input type="hidden" name="recipe_creator__background_color_contrast" value="<?= esc_attr(
-              get_option("recipe_creator__background_color_contrast")
-          ) ?>" />
+          <input type="hidden" name="recipe_creator__primary_color_contrast" value="<?php echo esc_attr(
+                                                                                      get_option("recipe_creator__primary_color_contrast")
+                                                                                    ) ?>" />
+          <input type="hidden" name="recipe_creator__primary_color_light" value="<?php echo esc_attr(
+                                                                                    get_option("recipe_creator__primary_color_light")
+                                                                                  ) ?>" />
+          <input type="hidden" name="recipe_creator__primary_color_light_contrast" value="<?php echo esc_attr(
+                                                                                            get_option("recipe_creator__primary_color_light_contrast")
+                                                                                          ) ?>" />
+          <input type="hidden" name="recipe_creator__primary_color_dark" value="<?php echo esc_attr(
+                                                                                  get_option("recipe_creator__primary_color_dark")
+                                                                                ) ?>" />
+          <input type="hidden" name="recipe_creator__secondary_color_contrast" value="<?php echo esc_attr(
+                                                                                        get_option("recipe_creator__secondary_color_contrast")
+                                                                                      ) ?>" />
+          <input type="hidden" name="recipe_creator__background_color_contrast" value="<?php echo esc_attr(
+                                                                                          get_option("recipe_creator__background_color_contrast")
+                                                                                        ) ?>" />
 
           <?php
           settings_fields("recipe_creator__general");
