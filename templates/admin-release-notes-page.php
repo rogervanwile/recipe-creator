ca4f86ea3f0757d617af7abfe21c84d4e370618f
Oliver Wagner <11217061+rogervanwile@users.noreply.github.com>
2024-01-26 18:45:03 +0100

    Replace Parsedown Changelogs with hardcoded changelogs

--- templates/admin-release-notes-page.php
+++ templates/admin-release-notes-page.php
@@ -2,29 +2,10 @@
 if (!defined('ABSPATH')) {
     die();
 }
+?>
 
-$Parsedown = new Parsedown(); ?>
 <div class="wrap recipe-creator--release-notes">
     <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
-    <?php
-    $userLocale = get_user_locale();
-
-    if (in_array($userLocale, ["de_DE", "de_AT", "de_CH"])) {
-        $changelog = file_get_contents(plugin_dir_path(__FILE__) . "../changelog-de_DE.md");
-    } else {
-        $changelog = file_get_contents(plugin_dir_path(__FILE__) . "../changelog.md");
-    }
-
-    // Replace some Placeholder in the markdown file
-    $changelog = str_replace(
-        ["##RECIPE_BLOCK_SETTINGS_LINK##", "##ASSETS_PATH##"],
-        [
-            get_admin_url(get_current_network_id(), "admin.php?page=recipe_creator"),
-            plugin_dir_url(__FILE__) . "../assets",
-        ],
-        $changelog
-    );
-
-    echo esc_html($Parsedown->text($changelog));
-    ?>
+    <h2><?php echo esc_html(__('2.0.0 - January 26, 2024', 'recipe-creator')); ?></h2>
+    <p><?php echo esc_html(__('We have renamed the plugin from "Foodblog-Toolkit" to "Recipe Creator". We want to give even more food bloggers the opportunity to find and use our plugin.', "recipe-creator")); ?></p>
 </div>
